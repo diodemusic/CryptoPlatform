3595ed4dc480f62678c81b11739a3c7d6b9e3fea
saves <diodemusicuk@gmail.com>
2025-02-20 00:00:53 +0000

    Refactor 👽

--- src/main.cpp
+++ src/main.cpp
@@ -3,68 +3,80 @@
 
 using namespace std;
 
-bool optionIsValid(int userOption, int max)
+const int numMenuOptions = 7;
+bool printAgain = true;
+string options[numMenuOptions] = {
+    "1: Print help",
+    "2: Print exchange stats",
+    "3: Make an offer",
+    "4: Make a bid",
+    "5: Print wallet",
+    "6: Continue",
+    "7: Exit"
+};
+string results[numMenuOptions] = {
+    "Help - your aim is to make money. Analyze the market and make binds and offers.",
+    "Market looks good.",
+    "Make an offer - enter the amount.",
+    "Make a bid - enter the amount.",
+    "Your wallet is empty.",
+    "Going to next time frame.",
+    "Exiting...",
+};
+
+bool inputIsValid(int userOption)
 {
-    if (userOption < 1 or userOption > max)
+    if (userOption < 1 or userOption > numMenuOptions)
     {
         return false;
     }
     return true;
 }
 
-void printOptions()
+void printMenu()
 {
-    int numMenuOptions = 7;
-    string options[numMenuOptions] = {
-        "1: Print help",
-        "2: Print exchange stats",
-        "3: Make an offer",
-        "4: Make a bid",
-        "5: Print wallet",
-        "6: Continue",
-        "7: Exit"
-    };
-    string results[numMenuOptions] = {
-        "Help - your aim is to make money. Analyze the market and make binds and offers.",
-        "Market looks good.",
-        "Make an offer - enter the amount.",
-        "Make a bid - enter the amount.",
-        "Your wallet is empty.",
-        "Going to next time frame.",
-        "Exiting...",
-    };
-    int userOption;
-    bool printAgain = true;
-
-    while (printAgain)
+    for (string option : options)
     {
-        for (string option : options)
-        {
-            cout << option << endl;
-        }
-
-        cout << "========== " << endl;
-        cout << "Press 1-" << numMenuOptions << endl;
-
-        cin >> userOption;
-
-        if (!optionIsValid(userOption, numMenuOptions))
-        {
-            cout << "Invalid choice. Choose 1-" << numMenuOptions << endl;
-            continue;
-        }
-
-        cout << results[userOption - 1] << endl;
-
-        if (userOption == 7)
-        {
-            printAgain = false;
-        }
+        cout << option << endl;
     }
+
+    cout << "============" << endl;
+    cout << "Press 1-" << numMenuOptions << endl;
+}
+
+int processInput()
+{
+    int userOption;
+    cin >> userOption;
+
+    if (userOption == 7)
+    {
+        printAgain = false;
+        return userOption;
+    }
+
+    if (inputIsValid(userOption))
+    {
+        return userOption;
+    }
+
+    cout << "Invalid input. Please press 1-" << numMenuOptions << endl;
+
+    return 0;
+}
+
+void printResponse()
+{
+    int userOption = processInput();
+    cout << results[userOption - 1] << endl;
 }
 
 int main()
 {
-    printOptions();
+    while (printAgain)
+    {
+        printMenu();
+        printResponse();
+    }
     return 0;
 }
