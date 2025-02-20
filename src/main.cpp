24aa3e15118f0bcfa38f791f8256c55405ba772b
saves <diodemusicuk@gmail.com>
2025-02-20 01:08:20 +0000

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,6 @@ using namespace std;
 
 const int numMenuOptions = 7;
 bool printAgain = true;
-string options[numMenuOptions] = {
-    "1: Print help",
-    "2: Print exchange stats",
-    "3: Make an offer",
-    "4: Make a bid",
-    "5: Print wallet",
-    "6: Continue",
-    "7: Exit"
-};
-string results[numMenuOptions] = {
-    "Help - your aim is to make money. Analyze the market and make binds and offers.",
-    "Market looks good.",
-    "Make an offer - enter the amount.",
-    "Make a bid - enter the amount.",
-    "Your wallet is empty.",
-    "Going to next time frame.",
-    "Exiting...",
-};
 
 bool inputIsValid(int userOption)
 {
@@ -33,15 +15,27 @@ bool inputIsValid(int userOption)
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         return false;
     }
+
     if (userOption < 1 or userOption > numMenuOptions)
     {
         return false;
     }
+
     return true;
 }
 
 void printMenu()
 {
+    string options[numMenuOptions] = {
+        "1: Print help",
+        "2: Print exchange stats",
+        "3: Make an offer",
+        "4: Make a bid",
+        "5: Print wallet",
+        "6: Continue",
+        "7: Exit"
+    };
+
     for (string option : options)
     {
         cout << option << endl;
@@ -51,7 +45,7 @@ void printMenu()
     cout << "Press 1-" << numMenuOptions << endl;
 }
 
-int processInput()
+int processUserOption()
 {
     int userOption;
     cin >> userOption;
@@ -67,15 +61,76 @@ int processInput()
         return userOption;
     }
 
-    cout << "Invalid input. Please press 1-" << numMenuOptions << endl;
-
     return 0;
+}
+
+void printHelp()
+{
+    cout << "Help - your aim is to make money. Analyze the market and make binds and offers." << endl;
+}
+
+void printExchangeStats()
+{
+    cout << "Market looks good." << endl;
+}
+
+void makeOffer()
+{
+    cout << "Make an offer - enter the amount." << endl;
+}
+
+void placeBid()
+{
+    cout << "Make a bid - enter the amount." << endl;
+}
+
+void printWallet()
+{
+    cout << "Your wallet is empty." << endl;
+}
+
+void goToNextTimeFrame()
+{
+    cout << "Going to next time frame." << endl;
+}
+
+void exitProgram()
+{
+    cout << "Exiting..." << endl;
+    exit(0);
 }
 
 void printResponse()
 {
-    int userOption = processInput();
-    cout << results[userOption - 1] << endl;
+    int userOption = processUserOption();
+    
+    switch (userOption)
+    {
+    case 1:
+        printHelp();
+        break;
+    case 2:
+        printExchangeStats();
+        break;
+    case 3:
+        makeOffer();
+        break;
+    case 4:
+        placeBid();
+        break;
+    case 5:
+        printWallet();
+        break;
+    case 6:
+        goToNextTimeFrame();
+        break;
+    case 7:
+        exitProgram();
+    
+    default:
+        cout << "Invalid input. Please press 1-" << numMenuOptions << endl;
+        break;
+    }
 }
 
 int main()
